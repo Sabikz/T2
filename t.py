6e340c09e03f551f335b3212cd6e349cf353d72e
Sabina <sabina.malikkizi@gmail.com>
2022-05-24 14:54:29 +0600

    initial commit

--- t.py
+++ t.py
@@ -1,30 +1,35 @@
-myDict = {
-    "postId": 1,
-    "id": 1,
-    "name": "id labore ex et quam laborum",
-    "email": "Eliseo@gardner.biz",
-    "body": "laudantium enim quasi est quidem magnam voluptate ipsam eostempora quo necessitatibus"
-}
 
-# 1) Вывести на экран "name"
-print(myDict["name"])
-# 2) Вывести длину значения "email"
-print(len(myDict["email"]))
-# 3) Вывести все ключ
-print(myDict.keys())
-# 4) Вывести все значения
-print(myDict.values())
-# 5) Вывести длину значения под ключом "body"
-print(len(myDict["body"]))
-# 6) Заменить пару "email" на "justcode@gmail.com" или на любую другую
-myDict["email"] = "justcode@gmail.com"
-print(myDict)
-# 7) Добавить пару дата и ее значение (14.05.2022)
-myDict["data"] = "14.05.2022"
-print(myDict)
-# 8) Увеличить значение id на +1
-myDict["id"] +=1
-print(myDict)
-# 9) Удалить пару "postId"
-del myDict["postId"]
-print(myDict)
+
+# Выводить на экран от -10 до +10
+# Выводить на экран от +10 до -10
+# Вывести все цифры от 1 до 100 через *2 (1, 2, 4, 8, 16 и т.д.)
+# Посчитать сумму цифр от 1 до 25
+
+def func(a):
+    if a == 11:
+        return
+    print(a)
+    return func(a+1)
+func(-10)
+
+def func1(b):
+    if b==-11:
+        return
+    print(b)
+    return func1(b-1)
+func1(10)
+
+def func2(c):
+    if c > 101:
+        return
+    print(c)
+    return func2(c*2)
+func2(1)
+
+def func3(d, s):
+    if d == 25:
+        return
+        print(s)
+    return func3(d+1), s+d
+func3(1,0)
+
